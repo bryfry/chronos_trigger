434548194baf50786ba4d57891289305ba4ad7e1
bryfry <bryon.fryer@gmail.com>
2017-02-13 15:18:32 -0500

    Update Chronos_Trigger.user.js

--- Chronos_Trigger.user.js
+++ Chronos_Trigger.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chronos Trigger
 // @namespace    https://github.com/bryfry/chronos_trigger
-// @version      0.2.22
+// @version      0.2.23
 // @description  Making WMKS.js Great Again!
 // @author       @bryfry
 // @match        http://ginkgo.azuretitan.com/*vm_view*
